ad778c93e754bfcc5ae2792eac654b7a8c38d7a3
Esther <esther191299@gmail.com>
2024-09-24 13:06:32 +0200

    initial commit

---
+++ src/main/java/org/example/seleniumpractise/SeleniumPractiseApplication.java
@@ -0,0 +1,13 @@
+package org.example.seleniumpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumPractiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/seleniumpractise/SeleniumPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seleniumpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumPractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
